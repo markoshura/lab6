5994fea457ea6206d22559a07d4e2a18783582be
markoshura <markoshura@yandex.ru>
2015-10-06 11:50:05 +0300

    Update A.py

--- A.py
+++ A.py
@@ -2,16 +2,14 @@ input=open('input.txt', 'r')
 output=open('output.txt', 'w')
 N = int(input.readline())
 s = list(map(int, input.readline().split()))
+i=0
+k=0
+while i<N:
+    j=0
+    while j<N:
+        if i!=j and (s[i]==s[j]):
+            k=s[i]
+        j+=1
+    i+=1
 
-f = False
-
-for i in range(N):
-    for j in range(N):
-        if i == j:
-            continue
-        if s[i] == s[j]:
-            f = True
-            break
-    if f:
-        break
-print(s[i], file=output)
+print(k, file=output)
