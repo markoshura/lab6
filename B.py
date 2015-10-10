6598daf1c39fe11a2621537a7ccaa818e3b9dceb
markoshura <markoshura@yandex.ru>
2015-10-10 16:16:23 +0300

    Update B.py

--- B.py
+++ B.py
@@ -1,15 +1,15 @@
-+input=open('input.txt', 'r')
-+output=open('output.txt', 'w')
-+N = int(input.readline())
-+lis = list(map(int, input.readline().split()))
-+nf = 0
-+hf = 0
-+for i in range(N):
-+    if lis[i] == 5:
-+        hf+= 1
-+    else:
-+        hf -= (lis[i]-5)//5
-+        if hf < 0:
-+            nf -= hf
-+            hf = 0
-+print(nf, file=output)
+input=open('input.txt', 'r')
+output=open('output.txt', 'w')
+N = int(input.readline())
+lis = list(map(int, input.readline().split()))
+nf = 0
+hf = 0
+for i in range(N):
+    if lis[i] == 5:
+        hf+= 1
+    else:
+        hf -= (lis[i]-5)//5
+        if hf < 0:
+            nf -= hf
+            hf = 0
+print(nf, file=output)
